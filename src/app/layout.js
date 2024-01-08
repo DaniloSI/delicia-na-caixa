2871683fe619a972503c5b7cd40202db29793ade
Danilo de Oliveira <danilo.oliveira@grupoboticario.com.br>
2024-01-08 09:10:20 -0300

    Add GA

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,33 +1,34 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { Toaster } from 'react-hot-toast'
-import { Flowbite } from 'flowbite-react'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "react-hot-toast";
+import { Flowbite } from "flowbite-react";
 
-const inter = Inter({ subsets: ['latin'] })
+import { GoogleTagManager } from '@next/third-parties/google'
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Delícia na caixa - fazer pedido',
-  description: 'Fazer pedido de salgados para festas e eventos',
-}
+  title: "Delícia na caixa - fazer pedido",
+  description: "Fazer pedido de salgados para festas e eventos",
+};
 
 const theme = {
   button: {
     color: {
-      primary: 'text-white bg-red-700 enabled:hover:bg-red-800 focus:ring-red-400',
-    }
-  }
-}
+      primary:
+        "text-white bg-red-700 enabled:hover:bg-red-800 focus:ring-red-400",
+    },
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head><meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /></head>
+    <html lang="pt-BR">
       <body className={inter.className}>
-        <Flowbite theme={{ theme }}>
-          {children}
-        </Flowbite>
+        <Flowbite theme={{ theme }}>{children}</Flowbite>
         <Toaster position="bottom-center" />
       </body>
+      <GoogleTagManager gtmId="G-ZYM0MPKM3D" />
     </html>
-  )
+  );
 }
