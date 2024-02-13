ec581b2a3ac235f449784a316d90adbd6d9e2694
Danilo de Oliveira <danilo.oliveira@grupoboticario.com.br>
2024-02-13 11:10:06 -0300

    ajustes de layout

--- src/components/SmallSavorySnacks.js
+++ src/components/SmallSavorySnacks.js
@@ -7,6 +7,7 @@ import TextInput from "./TextInput";
 import { HiMinusSm, HiPlusSm } from "react-icons/hi";
 import { CldImage } from "next-cloudinary";
 import StoreContext from "@/contexts/store";
+import { Button } from "flowbite-react";
 
 const SmallSavorySnacks = ({ name, namePlural, description, image }) => {
   const { control, setValue, getValues } = useFormContext();
@@ -17,55 +18,78 @@ const SmallSavorySnacks = ({ name, namePlural, description, image }) => {
   const fieldName = `snacks.${namePlural}`;
 
   const handleAdd = () => {
-    const value = Number(getValues(fieldName));
+    const value = getValues(fieldName);
     setValue(fieldName, value + 10);
   };
 
   const handleSubtract = () => {
-    const value = Number(getValues(fieldName));
-    setValue(fieldName, value > 10 ? value - 10 : "");
+    const value = getValues(fieldName);
+    setValue(fieldName, value > 10 ? value - 10 : 0);
   };
 
   return (
     <div className="flex bg-white flex-row max-w-xl gap-4 items-stretch">
       <CldImage
-        className="object-cover rounded-lg w-1/4"
+        className="object-cover rounded-lg w-1/6"
         src={"delicia-na-caixa/" + image}
         width="500"
         height="500"
         alt={name}
       />
 
-      <div className="flex flex-col grow justify-between leading-normal">
+      <div className="flex flex-col grow">
         <h5 className="text-lg font-bold tracking-tight text-gray-900 ">
           {name}
         </h5>
-        <p className="text-sm mb-1 font-normal text-gray-700 ">{description}</p>
         <div className="flex items-start">
-          <span className="text-xs grow">Unidade: {unitWeightInGrams}g</span>
-          <div className="flex gap-2">
-            <button type="button" onClick={handleSubtract}>
-              <HiMinusSm className="h-6 w-6 text-red-700" />
-            </button>
-            <Controller
-              render={({ field }) => (
+          <div className="grow">
+            <p className="text-sm mb-1 font-normal text-gray-700 ">
+              {description}
+            </p>
+            <div className="flex items-start">
+              <span className="text-xs grow">
+                Unidade: {unitWeightInGrams}g
+              </span>
+            </div>
+          </div>
+          <Controller
+            rules={{ valueAsNumber: true }}
+            render={({ field: { onChange, value, ...rest } }) => (
+              <div className="flex h-fit">
+                <Button
+                  size="xs"
+                  color="light"
+                  className={`border-none focus:ring-0 focus:bg-none hover:enabled:bg-color-none pt-2 ${
+                    value > 0 ? "" : "hidden"
+                  }`}
+                  onClick={handleSubtract}
+                >
+                  <HiMinusSm className="h-6 w-6 text-red-700" />
+                </Button>
                 <TextInput
-                  {...field}
+                  {...rest}
+                  value={value}
+                  onChange={onChange}
                   type="number"
                   placeholder="0"
                   inputMode="numeric"
                   sizing="xs"
-                  className="w-16"
+                  className={`w-16 ${value > 0 ? "" : "hidden"}`}
                 />
-              )}
-              name={fieldName}
-              control={control}
-              defaultValue=""
-            />
-            <button type="button" onClick={handleAdd}>
-              <HiPlusSm className="h-6 w-6 text-red-700" />
-            </button>
-          </div>
+                <Button
+                  size="xs"
+                  color="light"
+                  className="border-none focus:ring-0 focus:bg-none hover:enabled:bg-color-none pt-2"
+                  onClick={handleAdd}
+                >
+                  <HiPlusSm className="h-6 w-6 text-red-700" />
+                </Button>
+              </div>
+            )}
+            name={fieldName}
+            control={control}
+            defaultValue=""
+          />
         </div>
       </div>
     </div>
