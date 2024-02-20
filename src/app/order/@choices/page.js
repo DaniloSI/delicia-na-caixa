3e3feacf1ae65af8042b4acbbb1ed4ac04e7fb0d
Danilo de Oliveira <danilo.oliveira@grupoboticario.com.br>
2024-02-20 13:42:18 -0300

    fix view item list

--- src/app/order/@choices/page.js
+++ src/app/order/@choices/page.js
@@ -8,19 +8,25 @@ import MinimumQuantity from "@/components/MiniumQuantity";
 import Divider from "@/components/Divider";
 import StoreContext from "@/contexts/store";
 import { sendGAEvent } from "@next/third-parties/google";
+import { waitForConditionAndExecute } from "@/utils/wait-until";
 
 export default function Choices() {
   const { activeSnacks } = useContext(StoreContext);
 
   useEffect(() => {
-    sendGAEvent({
-      event: "view_item_list",
-      items: activeSnacks.map(snack => ({
-        item_id: snack.fieldName,
-        item_name: snack.name,
-        quantity: 1
-      }))
-    });
+    waitForConditionAndExecute(
+      () => window.gtag,
+      5000,
+      () => {
+        sendGAEvent({
+          event: "view_item_list",
+          items: activeSnacks.map(snack => ({
+            item_id: snack.fieldName,
+            item_name: snack.name,
+          }))
+        });
+      }
+    );
   }, [activeSnacks])
 
   return (
---
+++ src/utils/wait-until.js
@@ -0,0 +1,15 @@
+export function waitForConditionAndExecute(condition, timeout, callback) {
+  const checkInterval = 100; // check interval in milliseconds
+  let elapsedTimeout = 0;
+
+  const checkCondition = () => {
+      if (condition()) {
+          callback();
+      } else if (elapsedTimeout < timeout) {
+          elapsedTimeout += checkInterval;
+          setTimeout(checkCondition, checkInterval);
+      }
+  };
+
+  checkCondition();
+}
